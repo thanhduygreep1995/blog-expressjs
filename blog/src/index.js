f907e2db91ee94bc92dca54f8d66a103cb781349
thanhduygreep1995 <thanhduygreep1995@gmail.com>
2023-12-22 22:59:55 +0700

    bootstrap 4.6

---
+++ blog/src/index.js
@@ -0,0 +1,31 @@
+// import { engine } from 'express-handlebars';
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = 3000;
+const {engine} = require('express-handlebars');
+const morgan = require('morgan');
+// HTTP logger
+app.use(morgan('combined'));
+
+app.use(express.static(path.join(__dirname,'public')))
+
+// Template engine
+app.engine('hbs', engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', './views');
+app.set('views', path.join(__dirname,'resources\\views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
